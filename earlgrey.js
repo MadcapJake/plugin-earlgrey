de6474301b83c365f7533855f68058bfe585b2c8
Jake Russo <madcap.russo@gmail.com>
2015-08-09 15:41:27 -0500

    Use ES6 and move example to folder

--- earlgrey.js
+++
@@ -1,9 +0,0 @@
-var EarlGrey = require("earlgrey");
-
-exports.translate = function(load) {
-  var output = EarlGrey
-    .Generator({ es5: true, sourceMaps: "compute"})
-    .generate(EarlGrey.source(load.source, load.address))
-  load.source = output.code
-  load.metadata.sourceMap = output.map
-}
