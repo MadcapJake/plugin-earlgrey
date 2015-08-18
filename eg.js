f5afdff3531071a48e45498c2824acd72df3eaa6
Jake Russo <madcap.russo@gmail.com>
2015-08-18 16:52:28 -0500

    More work to get this working... :construction:

--- eg.js
+++ eg.js
@@ -5,18 +5,13 @@ var url = require("url");
 
 exports.translate = function (load) {
   console.log("systemjs translating " + load.address)
-  var source = eg.Source(load.source, url.parse(load.address).pathname);
-  var generator;
-  try {
-    generator = new eg.Generator({ es5: false, sourceMaps: "compute"});
-  } catch(err) { console.log("generator creation failed"); console.log(err); }
-  var result;
-  try {
-    result = generator.generate(source);
-  } catch(err) {
-    console.log("generator failed");
-    console.log(err);
-  }
+  var result,
+      generator,
+      source = eg.Source(load.source, url.parse(load.address).pathname);
+  try { generator = eg.Generator({ es5: false, sourceMaps: "compute"}); }
+  catch (e) { console.log(e); throw e; }
+  console.log("Generator built")
+  result = generator.generate(source);
   console.log("translation complete!");
   load.source = result.code;
   load.metadata.sourceMap = result.sourceMap;
